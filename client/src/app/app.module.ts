f9b728f0bedc97171fd5876eb0d70d298bc8883c
Wendy <Wendy@ad-ins.com>
2020-06-04 20:51:30 +0700

    End Of Section 11

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { RouterModule } from '@angular/router';
 import { HomeModule } from './home/home.module';
 import { HomeComponent } from './home/home.component';
@@ -9,7 +10,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,7 +23,13 @@ import { HttpClientModule } from '@angular/common/http';
     HomeModule,
     RouterModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ErrorInterceptor,
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,10 +2,30 @@ import { RouterModule } from '@angular/router';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TestErrorComponent } from './test-error/test-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
-  declarations: [NavBarComponent],
-  imports: [CommonModule, RouterModule],
+  declarations: [
+    NavBarComponent,
+    TestErrorComponent,
+    NotFoundComponent,
+    ServerErrorComponent,
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: false,
+      autoDismiss: true,
+      maxOpened: 3,
+      progressBar: true,
+      newestOnTop: false,
+    }),
+  ],
   exports: [NavBarComponent],
 })
 export class CoreModule {}
